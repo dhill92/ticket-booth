2f00abd2fdcfd57ce95c6c18fcc6b49f0d758ec3
dhill92 <dhill92@gmail.com>
2017-01-24 18:08:15 -0500

    first commit

---
+++ code.rb
@@ -0,0 +1,11 @@
+puts "Hello, and welcome to our amusement park!"
+
+puts "How many adult tickets can I get for you?"
+
+adult_tickets = gets.chomp
+
+puts "Ok, and how many child tickets?"
+
+child_tickets=gets.chomp
+
+puts "Here are your " + adult_tickets + " adult tickets and " + child_tickets + " child tickets!"
---
+++ food_code.rb
@@ -0,0 +1,10 @@
+hot_dog = 1.27
+hamburger = 4.17
+funnel_cake = 3.79
+ice_cream_sandwich = 0.75
+
+puts  hamburger * 2 + ice_cream_sandwich * 2
+
+puts funnel_cake * 8 + hot_dog
+
+puts (hot_dog + hamburger + funnel_cake + ice_cream_sandwich) * 3
